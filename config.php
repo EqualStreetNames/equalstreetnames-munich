3c215e3ad4fa84d89c82b371c880d438864ac0c3
Robin van der Linde <robin.vanderlinde@gmail.com>
2020-07-14 16:21:18 +0200

    Basic version

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'languages' => ['de', 'en', 'fr', 'nl'],
+
+    'exclude' => [
+        'relation' => [],
+        'way' => [],
+    ],
+
+    'gender' => [
+        'relation' => [],
+        'way' => [],
+    ],
+
+    'instances' => [
+        'Q5'        => true,  // human
+        'Q2985549'  => true,  // mononymous person
+        'Q20643955' => true,  // human biblical figure
+
+        'Q8436'     => false, // family
+        'Q101352'   => false, // family name
+        'Q327245'   => false, // team
+        'Q3046146'  => false, // married couple
+        'Q13417114' => false, // noble family
+    ],
+];
