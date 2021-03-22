51a812c73cc7584acef0e4f863675c26598d6560
ynux <ynux@gmx.net>
2021-03-22 16:34:25 +0100

    make it work for Friedenheimer Brücke

--- add_street_configphp.sh
+++ add_street_configphp.sh
@@ -15,7 +15,7 @@ JSONFILE=data/ways.geojson
 type jq >/dev/null 2>&1 || { echo >&2 "This script require jq but it's not installed."; exit 3; }
 
 function usage {
-   echo "Usage: $(basename $0) STREET GENDER COMMENT , e.g. Valpichlerstraße M \"eine der 42 Münchner Geiseln König Gustavs II. Adolf\" "
+   echo "Usage: $(basename $0) STREET GENDER NAME COMMENT , e.g. Valpichlerstraße M Valpichler \"eine der 42 Münchner Geiseln König Gustavs II. Adolf\" "
 }
 
 if [ "$#" -ne 3 ] || ! [ -f $JSONEFILE ]; then
@@ -25,10 +25,11 @@ fi
 
 STREET=$1
 GENDER=$2
-COMMENT=$3
-NAME=${STREET%%straße*}
+NAME=$3
+COMMENT=$4
+# NAME=${STREET%%straße*} // better do this manually, could re-add it as default
 
-for id in $(cat $JSONFILE | jq --arg street $STREET '.features[] | select(.properties.name==$street) | .id'); do
+for id in $(cat $JSONFILE | jq --arg street "${STREET}" '.features[] | select(.properties.name==$street) | .id'); do
   echo "            '$id' => '$GENDER', // $STREET: $NAME, $COMMENT "
 done
 
